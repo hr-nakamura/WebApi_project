c4969be6bcb53666eb91be7788b496ce4569bf54
WebApp <hr.naka.mr@hotmail.co.jp>
2021-07-15 20:43:10 +0900

    save

---
+++ WebApi_project/_home/debug/debug.ashx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace WebApi_project._home.debug
+{
+    /// <summary>
+    /// debug の概要の説明です
+    /// </summary>
+    public class Debug : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            //context.Response.Write("Hello World");
+
+            string LogData = context.Request.Form["LogData"];
+            LogData = Regex.Replace(LogData, "&lt;", "<");
+            LogData = Regex.Replace(LogData, "&gt;", ">");
+            string name= context.Request.Form["Name"];
+            if( name == "debug")
+            {
+                DebugHost.Debug.Write(LogData);
+            }
+            else if( name == "log")
+            {
+                DebugHost.Debug.WriteLog(LogData);
+            }
+            else
+            {
+                DebugHost.Debug.WriteErr(LogData);
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
