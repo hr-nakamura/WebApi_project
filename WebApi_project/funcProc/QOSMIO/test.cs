348e75d92e8d139d84bbf6c51590bb6fddca6f3b
WebApp <hr.naka.mr@hotmail.co.jp>
2021-08-19 15:06:20 +0900

    save

--- WebApi_project/funcProc/QOSMIO/test.cs
+++ WebApi_project/funcProc/QOSMIO/test.cs
@@ -17,34 +17,132 @@ namespace WebApi_project.hostProc
 {
     public class QOSMIO : hostProc
     {
+        int yymmAdd(int yymm, int mCnt)
+        {
+            int yy = yymm / 100;
+            int mm = yymm % 100;
+
+            int ym = (yy * 12) + mm;
+            ym += mCnt;
+            yy = ym / 12;
+            mm = ym % 12;
+            if (mm == 0) { yy--; mm = 12; }
+            return ((yy * 100) + mm);
+        }
+        int yymmDiff(int base_yymm, int yymm)
+        {
+            int b_yy = base_yymm / 100;
+            int b_mm = base_yymm % 100;
+            int yy = yymm / 100;
+            int mm = yymm % 100;
+
+            mm += (yy - b_yy) * 12;
+            int n = (mm - b_mm);
+            return (n);
+        }
+
+        int dayChk(DateTime d,int adjustDayCnt)
+        {
+            return (10);
+        }
+
+        string 確定日(int year, int? yosokuCnt)
+        {
+            int adjustDayCnt = 7;
+            //int? yosokuCnt = null;
+            //int year = 2021;
+
+
+            DateTime d = DateTime.Today;
+            int OKday = dayChk(d, adjustDayCnt);
+            int yymm = (d.Year * 100) + d.Month;
+            yymm = (d.Day < OKday ? yymmAdd(yymm, -1) : yymmAdd(yymm, 0));      // データ有効月の計算(12日以前は前々月)
+
+            int b_yymm = ((year - 1) * 100) + 10;
+
+            int actualCnt = yymmDiff(b_yymm, yymm);
+
+            if (actualCnt >= 12) actualCnt = 12;
+
+            if (!yosokuCnt.HasValue)        // nullの時
+            {
+                yosokuCnt = 12 - actualCnt;
+            }
+            else
+            {
+                if (yosokuCnt == 0)
+                {
+                    //	すべて計画
+                    actualCnt = 0;
+                    yosokuCnt = 0;
+                }
+                else if (yosokuCnt < 0)
+                {
+                    // 残り全て予測
+                    yosokuCnt = 12 - actualCnt;
+                }
+            }
+
+            string Buff = "";
+            var dispCnt = 12;
+            var s_yymm = ((year - 1) * 100) + 10;
+
+
+            // 次回の確定日のお知らせ
+            if (actualCnt >= 0 && actualCnt < dispCnt)
+            {
+                int x_yymm = yymmAdd(s_yymm, actualCnt);
+                int x_yy = (x_yymm / 100);
+                int x_mm = x_yymm % 100;
+                int xx_yymm = yymmAdd(x_yymm, 1);
+                int xx_yy = xx_yymm / 100;
+                int xx_mm = xx_yymm % 100;
+                int xx_dd = dayChk(new DateTime(xx_yy, xx_mm, 1), adjustDayCnt);
+
+                Buff = (x_yy + "年" + x_mm + "月の実績表示は" + xx_mm + "月" + xx_dd + "日以降です");
+            }
+            else if (actualCnt == dispCnt)
+            {
+                Buff = "(すべて実績データです)";
+            }
+            else
+            {
+                Buff = "実績データはありません";
+            }
+            return (Buff);
+        }
         void test()
         {
-            DateTime sDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month , 1);
+            int year = 2021;
+            int? yosokuCnt = null;
+            string Buff = 確定日(year,yosokuCnt);
+
+
+            //var a = 1;
+
+            /*
+                        DateTime sDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month , 1);
+                        DateTime eDate = sDate.AddMonths(1).AddDays(-1);
+                        DateTime curDate = sDate;
+
+                    //------------------------------------------------------------------
+                        curDate = sDate
+                       var n
+                       var dBuff = new Array()
+                       do
+                        {
+                            n = dBuff.length
+                          dBuff[n] = new Object;
+                            dBuff[n].日付 = JsFormatDateTime(curDate, 2)
+                          dBuff[n].曜日 = JsWeekday(curDate) - 1
+                          dBuff[n].offDay = (dBuff[n].曜日 == 0 || dBuff[n].曜日 == 6 ? 1 : 0)
+                          curDate = JsDateAdd("d", 1, curDate)
+                          } while (JsDay(curDate) != 1)
+
+*/
+
+            DateTime sDate = DateTime.Parse("2021/8/1");
             DateTime eDate = sDate.AddMonths(1).AddDays(-1);
-            DateTime curDate = sDate;
-
-            var a = 1;
-
-/*
-
-        //------------------------------------------------------------------
-            curDate = sDate
-           var n
-           var dBuff = new Array()
-           do
-            {
-                n = dBuff.length
-              dBuff[n] = new Object;
-                dBuff[n].日付 = JsFormatDateTime(curDate, 2)
-              dBuff[n].曜日 = JsWeekday(curDate) - 1
-              dBuff[n].offDay = (dBuff[n].曜日 == 0 || dBuff[n].曜日 == 6 ? 1 : 0)
-              curDate = JsDateAdd("d", 1, curDate)
-              } while (JsDay(curDate) != 1)
-
-
-
-            DateTime sDate = DateTime.Parse(s_sDate);
-            DateTime eDate = sDate.AddMonths(mCnt).AddDays(-1);
 
 
             string SQL = "";
@@ -60,21 +158,24 @@ namespace WebApi_project.hostProc
             sql.Replace("@sDate", SqlUtil.Parameter("string", sDate));
             sql.Replace("@eDate", SqlUtil.Parameter("string", eDate));
             SQL = sql.ToString();
-
+            DateTime d;
+            bool offDay;
             SqlDataReader reader = dbRead(DB, SQL);
             while (reader.Read())
             {
-
+                d = (DateTime)reader["日付"];
+                offDay= (bool)reader["offDay"];
             }
             reader.Close();
             DB.Close();
             DB.Dispose();
-*/
-            int year = 2020;
-            int s_yymm = ((year - 1) * 100 + 10);
-            int c_yymm = 202107;
-            int actualCnt = 10;
-            int yosokuCnt = 2;
+
+
+            //int year = 2020;
+            //int s_yymm = ((year - 1) * 100 + 10);
+            //int c_yymm = 202107;
+            //int actualCnt = 10;
+            //int yosokuCnt = 2;
         }
 
         public object json_projectTest(String Json)
