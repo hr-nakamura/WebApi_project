50638d1615797e7f3301289283f8b3cb30d39cbc
hr-nakamura <hr.naka.mr@hotmail.co.jp>
2021-10-07 16:11:21 +0900

    save

--- WebApi_project/hostProc/Entry.cs
+++ WebApi_project/hostProc/Entry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Xml;
+using System.Collections.Generic;
 
 using DebugHost;
 
@@ -48,6 +49,37 @@ namespace WebApi_project.hostProc
                 xmlDoc.AppendChild(error);
                 error.AppendChild(comment);
                 return (xmlDoc);
+            }
+            finally
+            {
+                //Debug.WriteErr("finally");
+            }
+        }
+        public object Entry_json(String Item, String Json)
+        {
+            object o_obj = new object();
+            try
+            {
+                string[] ItemWork = Item.Split('/');
+                string className = ItemWork[0];
+                string methodName = ItemWork[1];
+
+                String nameSpace = "WebApi_project.hostProc";
+
+                Type classType = Type.GetType(string.Concat(nameSpace, ".", className));
+                if (classType == null) throw new Exception("calss名[" + className + "]が不明です");
+                var obj = Activator.CreateInstance(classType);
+                MethodInfo method = classType.GetMethod(methodName);
+                if (method == null) throw new Exception("method名[" + methodName + "]が不明です");
+                o_obj = (object)method.Invoke(obj, new object[] { Json });
+
+                return (o_obj);
+            }
+            catch (Exception ex)
+            {
+                Dictionary<string, object> Tab = new Dictionary<string, object>();
+                Tab.Add("error_json", ex.Message);
+                return (Tab);
             }
             finally
             {
