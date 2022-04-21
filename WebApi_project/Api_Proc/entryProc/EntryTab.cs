e588ff60a11b53501a8103bfbb936e6400bff3fb
中村 博 <nakamura@eandm.co.jp>
2022-04-22 08:53:42 +0900

    save

--- WebApi_project/Api_Proc/entryProc/EntryTab.cs
+++ WebApi_project/Api_Proc/entryProc/EntryTab.cs
@@ -13,10 +13,10 @@ namespace WebApi_project.hostProc
     public partial class hostProc
     {
         public Dictionary<string, EntryInfo> projectInfo = new Dictionary<string, EntryInfo>() {
-            { "projectTest/test", new EntryInfo{
-                type = "text",
-                data = "http://localhost/Asp/Test/test.asp",
-                option = "{year:2022,actual:'漢字'}"
+            { "hostProc/memberInfo", new EntryInfo{
+                type = "method",
+                data = "hostProc/memberInfo",
+                option = "{memberID:451862'}"
                 }
             },
             { "hostProc/json_dayChk", new EntryInfo{
@@ -129,64 +129,5 @@ namespace WebApi_project.hostProc
             },
         };
 
-        public Dictionary<string, Dictionary<string, string>> EntryTabX = new Dictionary<string, Dictionary<string, string>>() {
-            { "projectTest",new Dictionary<string, string>(){
-                { "type", "method" },
-                { "func", "projectInfo/projectTest" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "projectBBS/projectList",new Dictionary<string, string>(){
-                { "type", "method" },
-                { "func", "projectBBS/projectList" },
-                { "option", "{beforBBS:'2021/06/30 00:00:00',visitBBS:'2021/07/07 10:12:12',limitYear:2019}" }
-            } },
-            { "projectCostProc/xml/projectInfo_XML_Detail",new Dictionary<string, string>(){
-                { "type", "xml" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/projectCostProc/xml/projectInfo_XML_Detail.asp" },
-                { "option", "{pNum:20214693}" }
-            } },
-            { "projectCostProc/xml/projectInfo_XML_Join",new Dictionary<string, string>(){
-                { "type", "xml" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/projectCostProc/xml/projectInfo_XML_Join.asp" },
-                { "option", "{pNum:20214693}" }
-            } },
-            { "projectCostProc/xml/projectInfoList_XML",new Dictionary<string, string>(){
-                { "type", "xml" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/projectCostProc/xml/projectInfoList_XML.asp" },
-                { "option", "{s_yymm:202110,mCnt:12}" }
-            } },
-
-
-            { "売上予測/売上予実_部門",new Dictionary<string, string>(){
-                { "type", "json" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_部門_JSON.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "売上予測/売上予実_分類",new Dictionary<string, string>(){
-                { "type", "json" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_分類_JSON.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "売上予測/売上予実_新規",new Dictionary<string, string>(){
-                { "type", "json" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_新規_JSON.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "売上予測/売上予実_新規2",new Dictionary<string, string>(){
-                { "type", "json" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_新規2_JSON.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "費用予測/費用状況",new Dictionary<string, string>(){
-                { "type", "json" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/費用予測/json/EMG費用状況_JSON.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-            { "要員情報/要員一覧",new Dictionary<string, string>(){
-                { "type", "xml" },
-                { "func", "http://kansa.in.eandm.co.jp/Project/要員情報/要員一覧/xml/要員一覧_XML.asp" },
-                { "option", "{year:2022,actual:5}" }
-            } },
-        };
     }
 }
