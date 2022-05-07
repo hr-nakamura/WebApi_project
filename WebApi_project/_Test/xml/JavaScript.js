953c98f17862958fa096c27cffd0c2623c1f335b
WebApp <hr.naka.mr@hotmail.co.jp>
2022-05-08 06:52:37 +0900

    save

---
+++ WebApi_project/_Test/xml/JavaScript.js
@@ -0,0 +1,22 @@
+﻿<%
+try{
+    var xTab = main(year, dispCnt, fixLevel)
+var x1 = 0;
+var x2 = 0;
+    var Tab = { "新規": [], "既存": [] };
+    for (var  key in xTab) {
+		if (key.indexOf("新規")) {
+			Tab["新規"][x1++] = xTab[key];
+            }
+		else {
+			Tab["既存"][x2++] = xTab[key];
+            }
+        }
+
+    $JsonOut_P(xTab);
+}catch(e){
+	pr(e.message)
+	}
+
+    Response.End
+%>
