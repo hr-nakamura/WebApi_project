2f65a09d31e9e49ec8406267c929151188b28b98
WebApp <hr.naka.mr@hotmail.co.jp>
2022-05-07 16:19:39 +0900

    save

--- WebApi_project/Api_Proc/entryProc/EntryTab.cs
+++ WebApi_project/Api_Proc/entryProc/EntryTab.cs
@@ -104,10 +104,10 @@ namespace WebApi_project.hostProc
                 }
             },
             { "売上予測/売上予実_新規2", new EntryInfoXml{
-                //type = "json",
-                //data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_新規2_JSON.asp",
-                type = "xml",
-                data = "http://localhost/test/_xmlData/売上予実_新規2.xml",
+                type = "json",
+                data ="http://localhost/Project/売上予測/json/売上予実_新規2_JSON.asp",
+                //type = "xml",
+                //data = "http://localhost/test/_xmlData/売上予実_新規2.xml",
                 option ="{year:2022,actual:6}"
                 }
             },
