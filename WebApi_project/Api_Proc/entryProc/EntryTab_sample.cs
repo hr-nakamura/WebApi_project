ba5d2389b3c71b17afcfb512f96ce9d15ff87c01
WebApp <hr.naka.mr@hotmail.co.jp>
2022-06-22 17:24:24 +0900

    save

--- WebApi_project/Api_Proc/entryProc/EntryTab_sample.cs
+++ WebApi_project/Api_Proc/entryProc/EntryTab_sample.cs
@@ -12,12 +12,50 @@ namespace WebApi_project.hostProc
 {
     public partial class hostProc
     {
-        //===================================================================================================================
-        public SortedDictionary<string, EntryInfoJson> XprojectInfo = new SortedDictionary<string, EntryInfoJson>() {
-            { "_sample/部門収支_json", new EntryInfoJson{
-                type = "json",
-                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_JSON.asp",
-                option = "{}"
+        //=========================================================================================
+        public SortedDictionary<string, EntryInfoXml> sample1_XML_部門収支 = new SortedDictionary<string, EntryInfoXml>() {
+            { "_sample/部門収支/統括収支計画_営業本部", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'統括詳細',secMode:'開発',dispName:'営業本部',year:2022,fix:70,yosoku:3}",
+                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
+                }
+            },
+            // dispCmd=統括一覧 にすると：遅い、
+            // dispName=指定しないと：全部持ってくる
+            { "_sample/部門収支/統括収支計画_第一開発本部", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'統括詳細',secMode:'開発',dispName:'第1開発本部',year:2022,fix:70,yosoku:3}",
+                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
+                }
+            },
+            { "_sample/部門収支/統括収支計画_第一開発本部2", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'統括個別',secMode:'開発',dispName:'第1開発本部',year:2022,fix:70,yosoku:3}",
+                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
+                }
+            },
+            { "_sample/部門収支/統括収支計画_本社", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'統括詳細',secMode:'開発',dispName:'本社',year:2022,fix:70,yosoku:3}",
+                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
+                }
+            },
+            { "_sample/部門収支/部収支計画", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'部門一覧',secMode:'開発',dispName:'',year:2022,fix:70,yosoku:5}",
+                dataX = "http://localhost/test/_xmlData/部収支計画.xml",
+                }
+            },
+            { "_sample/部門収支/課収支計画", new EntryInfoXml{
+                type = "xml",
+                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
+                option = "{dispCmd:'課一覧',dispName:'営業本部',secMode:'開発',year:2022,fix:70,yosoku:3}",
+                dataX = "http://localhost/test/_xmlData/課収支計画.xml",
                 }
             },
         };
@@ -61,39 +99,7 @@ namespace WebApi_project.hostProc
             },
         };
         //=========================================================================================
-        public SortedDictionary<string, EntryInfoXml> sample1_XML_部門収支 = new SortedDictionary<string, EntryInfoXml>() {
-            { "_sample/部門収支/統括収支計画_営業本部", new EntryInfoXml{
-                type = "xml",
-                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML.asp",
-                option = "{dispCmd:'統括詳細',secMode:'開発',dispName:'営業本部',year:2022,fix:70,yosoku:3}",
-                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
-                }
-            },
-            // dispCmd=統括一覧 にすると：遅い、
-            // dispName=指定しないと：全部持ってくる
-            { "_sample/部門収支/統括収支計画_営業本部2", new EntryInfoXml{
-                type = "xml",
-                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML.asp",
-                option = "{dispCmd:'統括詳細',secMode:'開発',dispName:'',year:2022,fix:70,yosoku:3}",
-                dataX = "http://localhost/test/_xmlData/統括収支計画.xml",
-                }
-            },
-            { "_sample/部門収支/部収支計画", new EntryInfoXml{
-                type = "xml",
-                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
-                option = "{dispCmd:'部門一覧',secMode:'開発',dispName:'',year:2022,fix:70,yosoku:5}",
-                dataX = "http://localhost/test/_xmlData/部収支計画.xml",
-                }
-            },
-            { "_sample/部門収支/課収支計画", new EntryInfoXml{
-                type = "xml",
-                data = "http://kansa.in.eandm.co.jp/Project/部門収支/xml/部門収支_XML_5.asp",
-                option = "{dispCmd:'課一覧',dispName:'営業本部',secMode:'開発',year:2022,fix:70,yosoku:3}",
-                dataX = "http://localhost/test/_xmlData/課収支計画.xml",
-                }
-            },
-        };
-        //=========================================================================================
+
         public SortedDictionary<string, EntryInfoXml> sample_XML_部門収支 = new SortedDictionary<string, EntryInfoXml>() {
             { "_sample/部門収支/EMG収支計画", new EntryInfoXml{
                 type = "xml",
