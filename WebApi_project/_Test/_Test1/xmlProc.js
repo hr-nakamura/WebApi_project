3b076126939d64c43db10a034dd33b867e04a67b
WebApp <hr.naka.mr@hotmail.co.jp>
2022-06-24 15:28:25 +0900

    save

--- WebApi_project/_Test/_Test1/xmlProc.js
+++ WebApi_project/_Test/_Test1/xmlProc.js
@@ -28,4 +28,47 @@ function xmlProc()
 			}
 		}
 	return(this)
-	}
+}
+function createElem(target_elem, elem_name, Tab) {
+    for (var i in Tab) {
+        var elem = document.createElement("div");
+        elem.setAttribute("class", elem_name);
+        elem.setAttribute("name", Tab[i]);
+        elem.setAttribute("xmlDoc", {});
+        elem.innerText = "data:" + Tab[i];
+        var target = document.getElementById(target_elem);
+        target.appendChild(elem);
+    }
+
+}
+function deleteElem(elem_name) {
+    do {
+        var elems = document.getElementsByClassName(elem_name);
+        for (var i = 0; i < elems.length; i++) {
+            var e = elems[0];
+            e.parentNode.removeChild(e);
+        }
+    } while (elems.length > 0);
+}
+function eventStart(elem_name, xml_name, xsl_name) {
+    var elems = document.getElementsByClassName(elem_name);
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].addEventListener('myEvent', function (event) {
+            //alert('buttonB:' + event.isTrusted);
+            var name = this.getAttribute("name");
+            var xmlDoc = this.getAttribute("xmlDoc");
+            xmlDoc = new ActiveXObject("Microsoft.XMLDom");
+            xmlDoc.async = false;
+            var ret = xmlDoc.load(xml_name);
+
+            var xslDoc = new ActiveXObject("Microsoft.XMLDom");
+            xslDoc.async = false;
+            var ret = xslDoc.load(xsl_name);
+            var Buff = xmlDoc.transformNode(xslDoc)
+            this.innerHTML = Buff;
+        });
+        var evt = document.createEvent('MouseEvents');
+        evt.initEvent('myEvent', false, true);
+        elems[i].dispatchEvent(evt);
+    }
+}
