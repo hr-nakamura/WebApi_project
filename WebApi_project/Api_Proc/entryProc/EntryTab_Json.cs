fdded9f7580d7b84b77eaadc2f12a39f2eca2f1a
WebApp <hr.naka.mr@hotmail.co.jp>
2022-10-03 11:46:22 +0900

    save

--- WebApi_project/Api_Proc/entryProc/EntryTab_Json.cs
+++ WebApi_project/Api_Proc/entryProc/EntryTab_Json.cs
@@ -13,7 +13,7 @@ namespace WebApi_project.hostProc
     public partial class hostProc
     {
         //===================================================================================================================
-        public SortedDictionary<string, EntryInfoJson> projectInfo = new SortedDictionary<string, EntryInfoJson>() {
+        public SortedDictionary<string, EntryInfoJson> projectInfo_JSON = new SortedDictionary<string, EntryInfoJson>() {
             { "projectInfo/memberInfo_json", new EntryInfoJson{
                 type = "method",
                 data = "hostProc/memberInfo_json",
@@ -29,7 +29,43 @@ namespace WebApi_project.hostProc
                 }
             },
         };
+// Jsonで作成したデータを確認するためのテーブル（実際はXMLに変換して使用する）
         public SortedDictionary<string, EntryInfoJson> sample_JSON = new SortedDictionary<string, EntryInfoJson>() {
+            { "売上予測/売上目標_部門", new EntryInfoJson{
+                type = "json",
+                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上目標_部門_JSON.asp",
+                option ="{year:2022,fix:70}",
+                dataX = "http://localhost/test/_jsonData/売上目標_部門_JSON.json",
+                }
+            },
+            { "売上予測/売上予実_部門", new EntryInfoJson{
+                type = "json",
+                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_部門_JSON.asp",
+                option ="{year:2022,fix:70}",
+                dataX = "http://localhost/test/_jsonData/売上予実_部門_JSON.json",
+                }
+            },
+            { "売上予測/売上予実_分類", new EntryInfoJson{
+                type = "json",
+                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_分類_JSON.asp",
+                option ="{year:2022,fix:70}",
+                dataX = "http://localhost/test/_jsonData/売上予実_分類_JSON.json",
+                }
+            },
+            { "売上予測/売上予実_新規", new EntryInfoJson{
+                type = "json",
+                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_新規_JSON.asp",
+                option ="{year:2022,fix:70}",
+                dataX = "http://localhost/test/_jsonData/売上予実_新規_JSON.json",
+                }
+            },
+            { "売上予測/売上予実_新規2", new EntryInfoJson{
+                type = "json",
+                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/売上予実_新規2_JSON.asp",
+                option ="{year:2022,fix:70}",
+                dataX = "http://localhost/test/_jsonData/売上予実_新規2_JSON.json",
+                }
+            },
             { "売上予測/グループ予実_部門", new EntryInfoJson{
                 type = "json",
                 data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/グループ予実_部門_JSON.asp",
@@ -40,15 +76,17 @@ namespace WebApi_project.hostProc
             { "売上予測/グループ予実_分類", new EntryInfoJson{
                 type = "json",
                 data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/グループ予実_分類_JSON.asp",
-                option ="{mode:'新規名',kind:'指定なし',yymm:202110,dispCnt:12,fix:70}",
+                option ="{mode:'新規名',kind:'新規顧客',yymm:202110,dispCnt:12,fix:70}",
                 dataX = "http://localhost/test/_jsonData/グループ予実_分類_JSON.json",
                 }
             },
-            { "売上予測/グループ予実_分類2", new EntryInfoJson{
+
+            { "費用予測/費用状況", new EntryInfoJson{
                 type = "json",
-                data ="http://kansa.in.eandm.co.jp/Project/売上予測/json/グループ予実_分類_JSON.asp",
-                option = "{mode:'分類名',kind:'指定なし',yymm:202110,dispCnt:12,actualCnt:7,fix:70}",
-                dataX = "http://localhost/test/_jsonData/グループ予実_分類_JSON.json",
+                data ="http://kansa.in.eandm.co.jp/Project/費用予測/json/EMG費用状況_JSON.asp",
+                option ="{year:2022,fix:70,actual:5}",
+                typeX = "xml",
+                dataX = "http://localhost/test/_xmlData/費用状況.xml",
                 }
             },
         };
